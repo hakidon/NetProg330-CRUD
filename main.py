730b15671bb5085e26a9663ce1e99e3c6f3eaf37
hakidon <mohdanihakim@gmail.com>
2023-01-23 12:11:30 +0800

    test db

--- main.py
+++ main.py
@@ -1,27 +1,77 @@
-#!/usr/bin/python
 import sqlite3
 
+from flask import Flask
+app = Flask(__name__) #creating the Flask class object   
+
 def connect_to_db():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('330_project.db')
     return conn
 
-def create_db_table():
+@app.route('/') 
+def test_con():
     try:
         conn = connect_to_db()
-        conn.execute('''
-            CREATE TABLE users (
-                user_id INTEGER PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL,
-                email TEXT NOT NULL,
-                phone TEXT NOT NULL,
-                address TEXT NOT NULL,
-                country TEXT NOT NULL
-            );
-        ''')
-
-        conn.commit()
-        print("User table created successfully")
+        return "Connection established!"
     except:
-        print("User table creation failed - Maybe table")
+        return "Fail"
     finally:
         conn.close()
+
+@app.route('/view') 
+def get_employee():
+    employees = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM employee_info")
+        rows = cur.fetchall()
+        return rows
+        # # convert row objects to dictionary
+        # for i in rows:
+        #     employee = {}
+        #     employee["employee name"] = i["employee name"]
+        #     employee["gender"] = i["gender"]
+        #     employee["email"] = i["email"]
+        #     employee["address"] = i["address"]
+        #     employee["Academic qualification"] = i["Academic qualification"]
+        #     employee["Username"] = i["Username"]
+        #     employee["Password"] = i["Password"]
+
+        #     employees.append(employee)
+
+    except:
+        employees = []
+        return 'asdas'
+
+    # return employees
+
+
+# def get_user_by_id(user_id):
+#     user = {}
+#     try:
+#         conn = connect_to_db()
+#         conn.row_factory = sqlite3.Row
+#         cur = conn.cursor()
+#         cur.execute("SELECT * FROM users WHERE user_id = ?", 
+#                        (user_id,))
+#         row = cur.fetchone()
+
+#         # convert row object to dictionary
+#         user["user_id"] = row["user_id"]
+#         user["name"] = row["name"]
+#         user["email"] = row["email"]
+#         user["phone"] = row["phone"]
+#         user["address"] = row["address"]
+#         user["country"] = row["country"]
+#     except:
+#         user = {}
+
+#     return user
+
+if __name__ =='__main__':  
+    app.run(debug = True)  
+
+
+
+    
