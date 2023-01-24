59b4eaf4447f44135cdd60030e380d759779c720
hakidon <mohdanihakim@gmail.com>
2023-01-24 19:47:39 +0000

    25 Jan 2023, 3:47 am

--- main.py
+++ main.py
@@ -64,8 +64,7 @@ def main():
         temp_success_insert = session.get('insert_employee', '')
         if temp_success_insert:
             session.pop('insert_employee', '')
-            return render_template('login.html', success_signup=1)
-        return render_template('login.html')
+        return render_template('login.html', success_signup=temp_success_insert)
     elif request.method == 'POST':
         name = request.form['username']
         passw = request.form['password']
